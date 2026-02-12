727dd15ef1922d2d0826297cffa0fe6316b41d8e
Ton Nom <tonemail@example.com>
2026-02-12 15:04:48 +0100

    creation Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+// src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer"; // Import du Footer
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Inscription from "./pages/Inscription";
@@ -8,11 +10,13 @@ import DashboardClient from "./pages/DashboardClient";
 import DashboardAdmin from "./pages/DashboardAdmin";
 import Subscriptions from "./pages/Subscriptions";
 
-
 function App() {
   return (
     <Router>
+      {/* Navbar en haut de toutes les pages */}
       <Navbar />
+
+      {/* Contenu des pages */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -20,8 +24,10 @@ function App() {
         <Route path="/dashboard-client" element={<DashboardClient />} />
         <Route path="/dashboard-admin" element={<DashboardAdmin />} />
         <Route path="/subscriptions" element={<Subscriptions />} />
-        
       </Routes>
+
+      {/* Footer en bas de toutes les pages */}
+      <Footer />
     </Router>
   );
 }
